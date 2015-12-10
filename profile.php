a3aea4fd8b671391b50697c5da39543965a42afe
tvreact <tvreact@gmail.com>
2015-12-10 10:24:27 +0100

    Mer saker i profil, samt forum.sql

--- profile.php
+++ profile.php
@@ -4,10 +4,15 @@
 	$profileid = $_GET["profile"];
 		
 	$sql = "SELECT * FROM users WHERE id='". $profileid ."'";
+	
 	if(mysqli_query($conn, $sql)){
+	
 		$sqlresult = mysqli_query($conn,$sql);
+		
 		if(mysqli_num_rows($sqlresult)>0) {
+		
 			$rad = mysqli_fetch_assoc($sqlresult);
+			
 			$avatar = $rad["avatar"];
 			$username = $rad['username'];
 			$profileid = $rad['id'];
@@ -18,14 +23,26 @@
 			} else {
 				$email = "No set email.";
 			}
+			$born = $rad['born'];
+			$gender = $rad['gender'];
+			$steamid = $rad['steamid'];
+			$bio = $rad['bio'];
+			$likes = $rad['likes'];
+			$signatur = $rad['signatur'];
 			
 			echo '<img src="'.$avatar.'" /><br>';
-			echo '<p>'.$username.'</p>';
-			echo '<p>'.$profileid.'</p>';
-			echo '<p>'.$usergroup.'</p>';
-			echo '<p>'.$regdate.'</p>';
-			echo '<p>'.$email.'</p>';
+			echo '<p>Användarnamn: '.$username.'</p>';
+			echo '<p>Profilid: '.$profileid.'</p>';
+			echo '<p>Användargrupp: '.$usergroup.'</p>';
+			echo '<p>Registreringsdatum: '.$regdate.'</p>';
+			echo '<p>Email: '.$email.'</p>';
+			echo '<p>Född: '.$born.'</p>';
+			echo '<p>Kön: '.$gender.'</p>';
+			echo '<p>SteamID: '.$steamid.'</p>';
+			echo '<p>Bio: '.$bio.'</p>';
+			echo '<p>Likes: '.$likes.'</p>';
+			echo '<p>Signatur: '.$signatur.'</p>';
+			
 		}
 	}
-	
 ?>
